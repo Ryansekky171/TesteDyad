301a37f2ee2aa1713b1d4ef09fd9e7f508f84c4a
[dyad] <git@dyad.sh>
2025-12-04 20:20:16 -0300

    [dyad] Corrigindo o erro useNavigate envolvendo o App com BrowserRouter - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import React from "react"; // Importar React
+import React from "react";
+import { BrowserRouter } from "react-router-dom"; // Importar BrowserRouter
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Adicionar BrowserRouter aqui */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
